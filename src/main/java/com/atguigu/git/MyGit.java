1bd6e656b2cc79f9b7ae46d65de000f2543f57bb
fuhan <1720476864@qq.com>
2021-12-24 14:53:54 +0800

    fuhan first commit

---
+++ src/main/java/com/atguigu/git/MyGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author
+ * @create 2021-12-24-14:50
+ */
+public class MyGit {
+
+    public static void main(String[] args) {
+        System.out.println("我爱我自己哈哈哈");
+    }
+}
